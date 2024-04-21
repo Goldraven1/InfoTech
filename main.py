c6cf38e5dede17c5fbcc90effa53aa4443c352db
goldraven <Kossmiks@mail.ru>
2024-04-21 16:12:29 +0300

    qwe

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import eel
+import psycopg2
+import bcrypt
 
 eel.init('static')
 
+@eel.expose
+def register(username, password):
+    conn = psycopg2.connect(
+        dbname="Info Tech", 
+        user="Postgre SQL 15", 
+        password="123", 
+        host="localhost"
+    )
+    cur = conn.cursor()
+
+    
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, hashed_password))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {'success': True}
 
 eel.start('index.html', size=(1920, 1080))
